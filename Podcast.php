7ee3d383d0c393e731a963cf7751936950df9134
gabss.md <bielg123br@gmail.com>
2026-05-26 20:17:27 -0300

    fix

---
+++ Podcast.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+class Podcast extends Midia {
+    private string $descricao;
+    private int $episodio;
+
+    public function __construct(string $descricao, int $episodio) {
+
+        $this->descricao = $descricao;
+        $this->episodio = $episodio;
+    }
+
+    public function getDescricao(): string {
+        return $this->descricao;
+    }
+
+    public function setDescricao($novoDescricao): void {
+        $this->descricao = $novoDescricao;
+    }
+
+    public function getEpisodio(): int {
+        return $this->episodio;
+    }
+
+    public function setEpisodio($novoEpisodio): void {
+        $this->episodio = $novoEpisodio;
+    }
+
+
+
+}
