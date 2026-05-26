24becd3e3fa443e39ed8de9348a75cbb33a73647
Luis Felipe de Oliveira Brasil <felipebrasil18102005@gmail.com>
2026-05-26 20:15:17 -0300

    commit 4

---
+++ Podcast.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+require_once 'Midia.php';
+require_once 'Reproduzivel.php';
+
+class Podcast extends Midia implements Reproduzivel {
+    private string $descricao;
+    private int $episodio;
+
+    public function __construct(string $descricao, int $episodio) {
+        $this->descricao = $novodescricao;
+        $this->episodio = $novoepisodio;
+    }
+
+    public function getDescricao(): string {
+        return $this->descricao;
+    }
+
+    public function setDescricao($novodescricao): void {
+        $this->descricao = $novodescricao;
+    }
+
+    public function getEpisodio(): int {
+        return $this->episodio;
+    }
+
+    public function setEpsodio($novoepisodio): void {
+        $this->episodio = $novoepisodio;
+    }
+
+
+
+}
