73a4ba89752e87d68c060976403f6ebdb6337102
Luis Felipe de Oliveira Brasil <felipebrasil18102005@gmail.com>
2026-05-26 20:14:23 -0300

    commit 4

--- Podcast.php
+++ Podcast.php
@@ -4,29 +4,29 @@ declare(strict_types=1);
 require_once 'Midia.php';
 require_once 'Reproduzivel.php';
 
-class Podcast extends Midia {
+class Podcast extends Midia implements Reproduzivel {
     private string $descricao;
     private int $episodio;
 
     public function __construct(string $descricao, int $episodio) {
-        $this->descricao = $novoDescricao;
-        $this->episodio = $novoEpisodio;
+        $this->descricao = $novodescricao;
+        $this->episodio = $novoepisodio;
     }
 
     public function getDescricao(): string {
         return $this->descricao;
     }
 
-    public function setDescricao($novoDescricao): void {
-        $this->descricao = $novoDescricao;
+    public function setDescricao($novodescricao): void {
+        $this->descricao = $novodescricao;
     }
 
     public function getEpisodio(): int {
         return $this->episodio;
     }
 
-    public function setEpsodio($novoEpisodio): void {
-        $this->episodio = $novoEpisodio;
+    public function setEpsodio($novoepisodio): void {
+        $this->episodio = $novoepisodio;
     }
 
 
