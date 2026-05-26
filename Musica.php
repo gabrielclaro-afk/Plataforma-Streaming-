54581466c870deffbcd51e0bb303f4a95d6a405e
gabss.md <bielg123br@gmail.com>
2026-05-26 20:09:40 -0300

    feat: adicao classe Musica

---
+++ Musica.php
@@ -0,0 +1,39 @@
+<?php
+declare (strict_types= 1);
+require_once 'Midia.php';
+    class Musica extends Midia {
+        private int $id;
+        private string $genero;
+        private string $album;
+
+        public function __construct(int $id, string $genero, string $album) {
+            $this->id = $id;
+            $this->genero = $genero;
+            $this->album = $album;
+        } 
+
+        public function getId() : int {
+            return $this->id;
+        }
+
+        public function getGenero() : string {
+            return $this->genero;
+        }
+
+        public function getAlbum() : string {
+            return $this->album;
+        }
+
+        public function setID(int $id) : void {
+            $this->id = $id;
+        }
+
+        public function setGenero(string $genero) : void {
+            $this->genero = $genero;
+        }
+
+        public function setAlbum(string $album) : void {
+            $this->album = $album;
+        }
+    }
+?>
